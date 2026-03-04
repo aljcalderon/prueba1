ba6a1414dc01dccccb4de360cc6f250034ab6843
aljcalderon <aljcalderon@Pablo>
2026-03-04 11:33:29 +0000

    V1.0 Refactorizacion

---
+++ src/main/java/com/mycompany/prueba1/ImpNomCant.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prueba1;
+
+/**
+ *
+ * @author aljcalderon
+ */
+public class ImpNomCant {
+    
+    protected static void printCuenta(String nombre, double cantidad) {
+        System.out.println("nombre: " + nombre);
+        System.out.println("cantidad: " + cantidad);
+    }
+    
+}
